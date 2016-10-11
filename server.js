a7962d7c7095c5f1b916950e8b0ca56608537dd3
bchardim <bchardim@redhat.com>
2016-10-11 20:37:01 +0200

    Fixed server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.status(200).send('ok-benja');
+  res.status(200).send('ok-benja-new');
 });
 
 var server = app.listen(3000, function () {
