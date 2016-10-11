a5fa76d60123f2de172d472f7267870e45e4f4a9
bchardim <bchardim@redhat.com>
2016-10-11 20:54:03 +0200

    Fixed server.js for new build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.status(200).send('ok-benja-new');
+  res.status(200).send('ok-benja-new-build');
 });
 
 var server = app.listen(3000, function () {
